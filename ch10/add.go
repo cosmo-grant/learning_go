af52ee6183c4a32a898a8c4db2c249916d80db0a
Cosmo Grant <cosmodgrant@gmail.com>
2025-12-31 16:59:57 +0000

    add ch10/ex2 (patch change)

--- ch10/add.go
+++ ch10/add.go
@@ -1,5 +1,12 @@
+// Package add provides a function for adding two ints.
+//
+// That's it.
+// For more on addition, see [this article].
+//
+// [this article]: https://www.mathisfun.com/numbers/addition.html
 package add
 
+// Add returns the sum of two ints.
 func Add(i, j int) int {
 	return i + j
 }
--- ch10_test/go.mod
+++ ch10_test/go.mod
@@ -2,4 +2,4 @@ module ch10_test
 
 go 1.24.1
 
-require github.com/cosmo-grant/learning_go/ch10 v1.0.0
+require github.com/cosmo-grant/learning_go/ch10 v1.0.1
